af759bcd1c3d85b290bf94f8d0c20fa507d856ab
plan-do-break-fix <jeffdswan@outlook.com>
2021-03-30 23:33:45 -0500

    fix - super lazy fix for bad existence check

--- rasterize.py
+++ rasterize.py
@@ -14,23 +14,23 @@ def rasterize(path_in, path_out, ext="png", width=None, height=None):
     for svg in svg_files:
         count += 1
         fpath_out = f"{path_out}/{svg[:-4]}.{ext}"
-        if os.path.isfile(fpath_out):
+        if not os.path.isfile(fpath_out):
+            fpath_in = f"{path_in}/{svg}"
+            if not width and height:
+                size = vg.size(f"{path_in}/{svg}")
+            _w = width if width else size[0]
+            _h = height if height else size[1]
+            try:
+                with Image(filename=fpath_in) as image:
+                    image.format = ext
+                    if not _w == image.size[0] or _h == image.size[1]:
+                        image.resize(_w, _h)
+                    image.save(filename=fpath_out)
+            except ImageError:
+                print(f"Unable to rasterize {svg}")
+                shutil.move(fpath_in, "/media/flagscraper/failed-rasterization/")
+            print(f"{count}/{len(svg_files)} | {path_out}/{svg[:-4]}.{ext} "\
+                  "written to disk")
+    else:
             print(f"{count}/{len(svg_files)} | {path_out}/{svg[:-4]}.{ext} "\
                    "already exists.")
-            break
-        fpath_in = f"{path_in}/{svg}"
-        if not width and height:
-            size = vg.size(f"{path_in}/{svg}")
-        _w = width if width else size[0]
-        _h = height if height else size[1]
-        try:
-            with Image(filename=fpath_in) as image:
-                image.format = ext
-                if not _w == image.size[0] or _h == image.size[1]:
-                    image.resize(_w, _h)
-                image.save(filename=fpath_out)
-        except ImageError:
-            print(f"Unable to rasterize {svg}")
-            shutil.move(fpath_in, "/media/flagscraper/failed-rasterization/")
-        print(f"{count}/{len(svg_files)} | {path_out}/{svg[:-4]}.{ext} "\
-              "written to disk")
