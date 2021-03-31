359b0a4e74fc1e8eed4560d4fcca9c5b6406f92b
plan-do-break-fix <jeffdswan@outlook.com>
2021-03-30 23:29:53 -0500

    features - error handling on ImageError; check if file exists, skip if does

--- rasterize.py
+++ rasterize.py
@@ -1,6 +1,7 @@
-import os
+import os, shutil
 from PIL import Image
 from wand.image import Image
+from wand.exceptions import ImageError
 
 import Vector as vg
 
@@ -12,16 +13,24 @@ def rasterize(path_in, path_out, ext="png", width=None, height=None):
     count = 0
     for svg in svg_files:
         count += 1
+        fpath_out = f"{path_out}/{svg[:-4]}.{ext}"
+        if os.path.isfile(fpath_out):
+            print(f"{count}/{len(svg_files)} | {path_out}/{svg[:-4]}.{ext} "\
+                   "already exists.")
+            break
+        fpath_in = f"{path_in}/{svg}"
         if not width and height:
             size = vg.size(f"{path_in}/{svg}")
         _w = width if width else size[0]
         _h = height if height else size[1]
-        fpath_in = f"{path_in}/{svg}"
-        fpath_out = f"{path_out}/{svg[:-4]}.{ext}"
-        with Image(filename=fpath_in) as image:
-            image.format = ext
-            if not _w == image.size[0] or _h == image.size[1]:
-                image.resize(_w, _h)
-            image.save(filename=fpath_out)
+        try:
+            with Image(filename=fpath_in) as image:
+                image.format = ext
+                if not _w == image.size[0] or _h == image.size[1]:
+                    image.resize(_w, _h)
+                image.save(filename=fpath_out)
+        except ImageError:
+            print(f"Unable to rasterize {svg}")
+            shutil.move(fpath_in, "/media/flagscraper/failed-rasterization/")
         print(f"{count}/{len(svg_files)} | {path_out}/{svg[:-4]}.{ext} "\
               "written to disk")
