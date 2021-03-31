f9d00f9049b3c5c6b310bebaf558083b097b152e
plan-do-break-fix <jeffdswan@outlook.com>
2021-03-30 21:16:22 -0500

    survey method wrapped in run() call

--- survey/survey.py
+++ survey/survey.py
@@ -3,9 +3,10 @@ import rasterize
 from survey import survey_bitmaps
 from survey import survey_vectors
 
-survey_vectors.run()
-rasterize("/media/flagscraper/svg",
-          "/media/flagscraper/png/128",
-          ext="png",
-          width=128, height=64)
-survey_bitmaps.run()
+def run():
+    survey_vectors.run()
+    rasterize("/media/flagscraper/svg",
+              "/media/flagscraper/png/128",
+              ext="png",
+              width=128, height=64)
+    survey_bitmaps.run()
