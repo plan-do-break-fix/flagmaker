bd5c4b056f2f9ea925a4da69ebaa32df18c2c6e8
plan-do-break-fix <jeffdswan@outlook.com>
2021-04-01 20:58:53 -0500

    Vector survey now build table of svg colors

--- survey/survey_vectors.py
+++ survey/survey_vectors.py
@@ -14,6 +14,16 @@ def run():
               "  md5 TEXT NOT NULL"
               ");"
     )
+    c.execute("CREATE TABLE IF NOT EXISTS 'colors' ("
+              "  rgba32hex TEXT NOT NULL);"
+    )
+    c.execute("CREATE TABLE IF NOT EXISTS 'svg_colors' ("
+              "  svg INTEGER NOT NULL,"
+              "  color INTEGER NOT NULL,"
+              "  FOREIGN KEY (svg) REFERENCES svgs (rowid),"
+              "  FOREIGN KEY (color) REFERENCES colors (rowid)"
+              ");"
+    )
     conn.commit()
     print("Database connection established.")
     count = 0
@@ -21,11 +31,25 @@ def run():
                 if _f.endswith("svg")
                 and os.path.isfile(f"/media/flagscraper/svg/{_f}")]:
         count +=1
+        c.execute("SELECT rowid FROM svgs WHERE fname=?", (svg))
+        if c.fetchone():
+            print(f"Skipping file {count} | {svg}")
+            break
         print(f"Processing file {count} | {svg}")
         with open(f"/media/flagscraper/svg/{svg}") as _f:
             data = _f.read()
         md5hex = md5(data.encode()).hexdigest()
-        ncolors = len(vg.colors(f"/media/flagscraper/svg/{svg}"))
+        color_list = vg.colors(f"/media/flagscraper/svg/{svg}")
+        ncolors = len(color_list)
         c.execute("INSERT INTO svgs (fname, md5, ncolors) VALUES (?,?,?)",
                                     (svg, md5hex, ncolors))
-        conn.commit()
+        svg_pk = c.lastrowid
+        for color in color_list:
+            c.execute("SELECT rowid FROM colors WHERE rgba32hex=?", (color,))
+            color_pk = c.fetchone()
+            if not color_pk:
+                c.execute("INSERT INTO colors (rgba32hex) VALUES (?)", (color,))
+                color_pk = c.lastrowid
+            c.execute("INSERT INTO svg_colors (svg, color) VALUES (?,?)",
+                                              (svg_pk, color_pk))
+            conn.commit()
