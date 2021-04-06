3115550d23886944f95cc91a2ecc62b2cd389e82
plan-do-break-fix <jeffdswan@outlook.com>
2021-04-06 11:59:44 -0500

    fix - convert bool to int to match db schema; modified print statement

--- survey/survey.py
+++ survey/survey.py
@@ -9,14 +9,14 @@ def survey(dataset: str, datapath="/media/flagmaker/"):
     db = SqliteInterface.Interface(dataset)
     ftypes = get_ftypes(path)    
     if len(ftypes) > 1:
-        print("I have not been programmed to do that yet. Blame Jeff.")
+        print("I have not been programmed to do that.")
         return False
     for image in [f for f in os.listdir(path)
                   if os.path.isfile(f) and f.endswith(ftypes[0])]:
         with open(image) as _f:
             md5hash = md5(_f).hexdigest()
         if ftypes[0] in ["bmp", "png", "jpg"]:
-            transparency = filters.bitmap.no_transparency(path)
+            transparency = int(filters.bitmap.no_transparency(path))
             db.record_bitmap(fname, transparency, md5hash)
         elif ftypes[0] == "svg":
             colors = survey.vector.colors(path)
