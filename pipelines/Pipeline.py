c7e2670a285f6372ec429bc78ccc18b7a466f49f
plan-do-break-fix <jeffdswan@outlook.com>
2021-04-06 13:52:12 -0500

    feature - piplines - AbstractPipeline class to automate certain file handling tasks, simplify image set transformation specification, and serve as a record of those specs

---
+++ pipelines/Pipeline.py
@@ -0,0 +1,29 @@
+import logging, os, tarfile
+from survey.survey import survey
+
+
+class AbstractPipeline:
+
+    def __init__(self):
+        self.input = ""
+        self.output = ""
+        self.to_process = []
+
+    def run(self) -> None:
+        if os.path.isdir(f"{datapath}/{output_name}") \
+                   or os.path.isfile(f"{datapath}/{output_name}.tar"):
+            print(f"Data set named {self.output} already exists.")
+            return False
+        self.load_data_set()
+        self.process()
+        survey(self.output)
+
+    def load_data_set(self) -> bool:
+        """Populates Pipeline.to_process with image file paths."""
+        _path = f"{datapath}/{input_name}"
+        if not os.path.isdir(_path):
+            if not os.path.isfile(f"{_path}.tar"):
+                raise RuntimeError
+            with open(f"{_path}.tar") as _tar:
+                _tar.extractall(path=_path)
+        self.to_process = [_f for _f in os.listdir(_path) if os.path.isfile(_f)]
---
+++ pipelines/wikipedia_2021_03_svg_RAW_filtered.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+import os
+
+from pipelines.Pipeline import AbstractPipeline
+from transforms.rasterize import rasterize
+
+
+class Pipeline(AbstractPipeline):
+
+    def __init__(self, datapath="/media/flagmaker"):
+        super().__init__()
+        self.input = "wikipedia.2021.03-svg.RAW"
+        self.output = "wikipedia.2021.03-svg.RAW:filtered"
+
+    def process(self):
+        os.makedirs(f"{datapath}/{self.output}")
+        while self.to_process:
+            image = self.to_process.pop()
+            print(f"Processing {image}")
+            rasterize(fpath_in=f"{datapath}/{self.input}/{image}", 
+                      path_out=f"{datapath}/{self.output}",
+                      width=128, height=64)
+        self.survey(self.output)
+
+
+
+
+
+
